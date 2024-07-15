027e76d1d5a62a099f623746176435bd0d571716
flowersought <flowersought@gmail.com>
2024-07-15 14:20:07 +0800

    3

--- three/3-1.py
+++ three/3-1.py
@@ -1,14 +1,24 @@
 with open('3.txt','r') as inputfile:
     input=inputfile.read()
-    lines=input.split('\n')
+    lines='.'+input.split('\n')+'.'
 
 lineno=0
 symbols='!@#$%^&*-=+_'
-coords=[]
+nums='0123456789'
+symb=[]
+numb=[]
+ok=[]
+
+print(lines)
 
 for line in lines:
     for i in range(0,len(line)):
         if line[i] in symbols:
-            coords.append((i,lineno))
+            symb.append(i,lineno)
+        if line[i] in nums:
+            numb.append(i,lineno)
     lineno+=1
 
+for line in lines:
+    for (x,y) in nums: 
+        if 
