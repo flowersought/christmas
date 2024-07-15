0e648880c8e3ca3908108eea022fd8a3abc5efb1
flowersought <flowersought@gmail.com>
2024-07-15 16:20:29 +0800

    im goign to commit something else

--- 2023/three/3-1.py
+++ 2023/three/3-1.py
@@ -1,24 +1,51 @@
+#I FORGOT TO ADD / INTO THTE SYMBOLS LIST KMS 
+
 with open('3.txt','r') as inputfile:
     input=inputfile.read()
-    lines='.'+input.split('\n')+'.'
+    lines=input.split('\n')
 
-lineno=0
-symbols='!@#$%^&*-=+_'
-nums='0123456789'
-symb=[]
-numb=[]
-ok=[]
-
-print(lines)
-
+grid=[]
 for line in lines:
-    for i in range(0,len(line)):
+    grid.append("."+line+".")
+
+#[y value][x value]
+
+symbols='!@#$%^&*-_=+/'
+nums='1234567890'
+scoords=[]
+morecoords=[]
+numbers=[]
+total=0
+
+for lineno,line in enumerate(lines):
+    for i in range(len(line)):
         if line[i] in symbols:
-            symb.append(i,lineno)
-        if line[i] in nums:
-            numb.append(i,lineno)
-    lineno+=1
+            scoords.append((lineno,i))
 
-for line in lines:
-    for (x,y) in nums: 
-        if 
+for [i,j] in scoords:
+    for y in range(i-1,i+2):
+        for x in range(j-1,j+2):
+            morecoords.append((y,x))
+# print(morecoords)
+
+for y,line in enumerate(grid):
+    temp=''
+    coo=[]
+    for x in range(len(grid[y])):
+        if line[x] in nums:
+            temp+=line[x]
+            coo.append((y,x-1)) 
+        elif temp!='' and [temp,coo] not in numbers:
+            numbers.append([temp,coo])
+            temp=''
+            coo=[]
+
+for num,coords in numbers:
+    for i in range(len(coords)):
+        if coords[i] in morecoords:
+            total+=int(num)
+            print(num)
+            break
+
+print(total)
+
